ef41de5fd5cf39bdb951fd953d08eba498b12c66
Visha1225 <146875366+Visha1225@users.noreply.github.com>
2025-02-10 12:14:06 -0800

    Add files via upload

---
+++ BMI_calculation.py
@@ -0,0 +1,27 @@
+weight = float(input('enter your weight in kg'))
+height = float(input('enter your height in meters'))
+
+height_in_meters = height/100
+BMI = weight/ (height_in_meters**2)
+print(BMI)
+
+if(BMI<16):
+    print("you are severely underweight"),BMI
+
+elif(BMI>=16 and BMI<18.5):
+    print("you are underweight"),BMI
+
+elif(BMI>=18.5 and BMI<24):
+    print ("you are healty"),BMI
+
+elif(BMI>=24 and BMI<30):
+    print("you are overweight"),BMI
+
+elif(BMI>=30):
+    print("you are obese"),BMI
+
+
+
+
+
+  
